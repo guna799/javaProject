62b39b94cd6cac77a9a55217fd6fed45db068230
guna <gunavardhan799@gmail.com>
2022-12-26 10:25:52 +0530

    choice commmit

---
+++ choice.groovy
@@ -0,0 +1,25 @@
+properties([parameters([booleanParam('if'), choice(choices: ['equal ', 'less', 'more'], name: 'Choices')])])
+pipeline
+{
+    agent admin
+    stages
+    {
+        stage("boolean")
+        {
+            steps
+            {
+                echo 'this is boolean parameters'
+                echo 'this code is form git'
+                echo "${"if"}
+            }
+        }
+        stage("choice")
+        {
+            steps
+            {
+                ehco ' this is form choice parameters'
+                echo "${Choices}
+            }
+        }
+    }
+}
